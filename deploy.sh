ea2c1a8d65c80c42a5b74f3e2e772612ce2fefb1
Alex <alexbrucely@gmail.com>
2022-08-11 23:35:57 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -31,6 +31,66 @@ for i in $(echo ${environment_overlays} | sed "s/,/ /g"); do
   sleep 5
 done
 
+# register clusters to gloo mesh with helm
+
+until [ "${SVC}" != "" ]; do
+  SVC=$(kubectl --context ${mgmt_context} -n gloo-mesh get svc gloo-mesh-mgmt-server -o jsonpath='{.status.loadBalancer.ingress[0].*}')
+  echo waiting for gloo mesh management server LoadBalancer IP to be detected
+  sleep 2
+done
+
+kubectl apply --context ${cluster_context} -f- <<EOF
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: gm-enterprise-agent-${cluster_context}
+  namespace: argocd
+spec:
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: gloo-mesh
+  source:
+    repoURL: 'https://storage.googleapis.com/gloo-mesh-enterprise/gloo-mesh-agent'
+    targetRevision: ${gloo_mesh_version}
+    chart: gloo-mesh-agent
+    helm:
+      valueFiles:
+        - values.yaml
+      parameters:
+        - name: cluster
+          value: '${cluster_context}'
+        - name: relay.serverAddress
+          value: '${SVC}:9900'
+        - name: relay.authority
+          value: 'gloo-mesh-mgmt-server.gloo-mesh'
+        - name: relay.clientTlsSecret.name
+          value: 'gloo-mesh-agent-cluster1-tls-cert'
+        - name: relay.clientTlsSecret.namespace
+          value: 'gloo-mesh'
+        - name: relay.rootTlsSecret.name
+          value: 'relay-root-tls-secret'
+        - name: relay.rootTlsSecret.namespace
+          value: 'gloo-mesh'
+        - name: rate-limiter.enabled
+          value: 'false'
+        - name: ext-auth-service.enabled
+          value: 'false'
+        # enabled for future vault integration
+        - name: istiodSidecar.createRoleBinding
+          value: 'true'
+  syncPolicy:
+    automated:
+      prune: false
+      selfHeal: false
+    syncOptions:
+    - Replace=true
+    - ApplyOutOfSyncOnly=true
+  project: default
+EOF
+
+# wait for completion of bookinfo install
+./tools/wait-for-rollout.sh deployment productpage-v1 bookinfo-frontends 10 ${cluster_context}
+
 # echo port-forward commands
 echo
 echo "access argocd dashboard:"
