35bb513c04f059124015ff5a5dd715941a98c700
Alex <alexbrucely@gmail.com>
2022-09-12 17:31:49 -0700

    Update test.sh

--- environment/wave-6-httpbin/test.sh
+++ environment/wave-6-httpbin/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cluster_context="cluster1"
+cluster_context="cluster2"
 
 # wait for completion of httpbin install
 ./tools/wait-for-rollout.sh deployment in-mesh httpbin 10 ${cluster_context}
