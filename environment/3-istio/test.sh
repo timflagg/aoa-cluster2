33f28f4696e8b3b2eb29c070b00d2ae52739b4df
Alex <alexbrucely@gmail.com>
2022-12-09 15:38:16 -0800

    fix namespace

--- environment/3-istio/test.sh
+++ environment/3-istio/test.sh
@@ -4,6 +4,6 @@ if [ "${environment_overlay}" == "ilcm" ]; then
      echo ""
   else 
      #./tools/wait-for-rollout.sh deployment istio-ingressgateway-1-15 istio-gateways 10 ${cluster_context}
-     ./tools/wait-for-rollout.sh deployment istio-eastwestgateway-1-15 istio-eastwest 10 ${cluster_context}
+     ./tools/wait-for-rollout.sh deployment istio-eastwestgateway-1-15 istio-gateways 10 ${cluster_context}
   fi
 
